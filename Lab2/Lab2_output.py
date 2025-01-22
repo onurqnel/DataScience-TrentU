af1700f3761970e4270d3201fef52f7808741288
onurqnel <onur.onel2230@gmail.com>
2025-01-22 18:27:23 -0500

    Lab 2 Completed

---
+++ Lab2/Lab2_output.py
@@ -0,0 +1,66 @@
+Python 3.13.1 (main, Dec  4 2024, 18:05:56) [GCC 14.2.1 20240910] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========= RESTART: /home/onel/git/COIS1400/Lab2/FloatingPointIssues.py =========
+
+Part 2
+
+Iteration:  10
+Expected Sum: 1.0
+Actual Sum:  0.9999999999999999
+Error:  1.1102230246251565e-16
+
+Part 4
+
+Iteration:  10
+Expected Sum: 1.000000000000000055511151231
+Actual Sum:  1.000000000000000055511151231
+Error:  0E-27
+
+========= RESTART: /home/onel/git/COIS1400/Lab2/FloatingPointIssues.py =========
+
+Part 2
+
+Iteration:  100
+Expected Sum: 10.0
+Actual Sum:  9.99999999999998
+Error:  1.9539925233402755e-14
+
+Part 4
+
+Iteration:  100
+Expected Sum: 10.00000000000000055511151231
+Actual Sum:  10.00000000000000055511151230
+Error:  1E-26
+>>> 
+========= RESTART: /home/onel/git/COIS1400/Lab2/FloatingPointIssues.py =========
+
+Part 2
+
+Iteration:  1000
+Expected Sum: 100.0
+Actual Sum:  99.9999999999986
+Error:  1.4068746168049984e-12
+
+Part 4
+
+Iteration:  1000
+Expected Sum: 100.0000000000000055511151231
+Actual Sum:  100.0000000000000055511151203
+Error:  2.8E-24
+>>> 
+========= RESTART: /home/onel/git/COIS1400/Lab2/FloatingPointIssues.py =========
+
+Part 2
+
+Iteration:  10000
+Expected Sum: 1000.0
+Actual Sum:  1000.0000000001588
+Error:  -1.588205122970976e-10
+
+Part 4
+
+Iteration:  10000
+Expected Sum: 1000.000000000000055511151231
+Actual Sum:  1000.000000000000055511151020
+Error:  2.11E-22
