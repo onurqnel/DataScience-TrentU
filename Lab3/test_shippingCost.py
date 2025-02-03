1c97f4bec2b57d11dfe4b724a4a730a1b3de04a5
onurqnel <onur.onel2230@gmail.com>
2025-02-02 21:48:24 -0500

    Update

--- Lab3/test_shippingCost.py
+++ Lab3/test_shippingCost.py
@@ -1,28 +1,54 @@
+# Student Name:   Onur Onel
+# Student No:     0865803
+# Trent Email:    onuronel@trentu.ca
+# Date:           01.28.2025
+# Lab 3: Debugging and Testing
+
+# This Lab demonstrates the implementation of debugging techniques 
+# and creating test cases using the unittest framework.
+
+# Importing the unittest module 
 import unittest
+
+# Importing the shippingCost 
 import shippingCost
 
+# Defining a test class
 class TestShippingCost(unittest.TestCase):
-    #first test case
+    
+    # First test case: Test calculateCost with input 11
+    # Expected output: 4.75
     def test_shippingCostA(self):
         result = shippingCost.calculateCost(11)
         self.assertEqual(result, 4.75)
-    #second test case
+
+    # Second test case: Test calculateCost with input 7
+    # Expected output: 4.00
     def test_shippingCostB(self):
         result = shippingCost.calculateCost(7)
         self.assertEqual(result, 4.00)
-    #third test case
+
+    # Third test case: Test calculateCost with input 3
+    # Expected output: 3.00
     def test_shippingCostC(self):
         result = shippingCost.calculateCost(3)
         self.assertEqual(result, 3.00)
-    #fourth test case
+
+    # Fourth test case: Test calculateCost with input 1
+    # Expected output: 1.50
     def test_shippingCostD(self):
         result = shippingCost.calculateCost(1)
         self.assertEqual(result, 1.50)
-    #edge case
+
+    # Edge case one: Test calculateCost with negative input (-3113)
+    # Expected output: 1.50 
     def test_shippingCostE(self):
         result = shippingCost.calculateCost(-3113)
         self.assertEqual(result, 1.50)
-    #edge case
+
+    # Edge case two: Test calculateCost with input 0
+    # Expected output: 1.50 
     def test_shippingCostF(self):
         result = shippingCost.calculateCost(0)
         self.assertEqual(result, 1.50)
+
---
+++ Lab4/MatrixMatrixMultiplication.py
@@ -0,0 +1,65 @@
+# Student Name:   Onur Onel
+# Student No:     0865803
+# Trent Email:    onuronel@trentu.ca
+# Date:           01.22.2025
+
+# Lab 4: Matrix Multiplication Runtime Measurements
+
+# Optimizing matrix multiplication is important for performance in AI, graphics, and scientific computing.
+# The standard matrix multiplication algorithm, based on its mathematical definition, has a time complexity of O(n^3), but faster algorithms exist.
+# For example, Strassen’s algorithm reduces the complexity to O(n^2.8), and even more advanced methods achieve further improvements.
+
+# In order to correctly multiply two matrices:
+# - The number of columns in the first matrix must be equal to the number of rows in the second matrix.
+# - Since there is a shared property between the two matrices, we need three loop variables (i, j, k).
+# - Matrix multiplication is not commutative: (Matrix A × Matrix B != Matrix B × Matrix A).
+# - Matrix multiplication is associative: (A × B) × C = A × (B × C).
+# - Matrix multiplication is distributive: A × (B + C) = A × B + A × C.
+
+import random
+import time
+from memory_profiler import profile
+
+# Multiplication of two matrices using the standard algorithm
+def multiply():
+    # Iterate over each row of matrix P
+    for i in range(size):
+        # Iterate over each column of matrix P
+        for j in range(size): 
+            # Iterate over the shared dimension of matrices A and B
+            for k in range(size):
+                P[i][j] += (A[i][k] * B[k][j]) # P(IxJ) = A(IxK) * B(KxJ)
+    return P # Return the result matrix P
+
+size=int(input("Enter size: "))
+
+r1=size
+c1=size
+r2=size
+c2=size
+
+
+A=[[0 for i in range(c1)] for j in range(r1)] #initialize matrix A
+
+#input matrix A
+for i in range(r1):
+    for j in range(c1):
+        x=random.randint(1,100)
+        A[i][j]=x
+
+B=[[0 for i in range(c2)] for j in range(r2)] #initialize matrix B
+
+#input matrix B
+for i in range(r2):
+    for j in range(c2):
+        x=random.randint(1,100)
+        B[i][j]=x
+
+P=[[0 for i in range(c2)] for j in range(r1)] #initialize product matrix
+#INSERT CODE TO START TIMER 
+print(multiply()) 
+#INSERT CODE TO STOP TIMER
+#INSERT CODE TO COMPUTE ELAPSED TIME
+#INSERT CODE TO PRINT OVERALL TIME (IN s, NUMBER OF ELEMENTS IN THE RESULT MATRIX, AND TIME PER ELEMENT (IN ms)
+
+
