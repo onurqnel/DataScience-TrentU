f704f70b05fa46d05fa86627199a8df0e111a407
onurqnel <onur.onel2230@gmail.com>
2025-03-06 21:49:36 -0500

    Updates

---
+++ Clustering/k-means.py
@@ -0,0 +1,12 @@
+# Student Name:   Onur Onel
+# Student No:     0865803
+# Trent Email:    onuronel@trentu.ca
+# Date:           07.04.2025
+# Lab 8:          Clustering
+
+import sklearn
+import numpy as np  
+import matplotlib.pyplot as plt  
+from pydataset import data  
+
+iris = data('iris')  
