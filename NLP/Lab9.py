f83ac2690f4e56bd998d68ac007e8e8a73776c8d
onurqnel <onur.onel2230@gmail.com>
2025-03-13 21:50:12 -0400

    Updates

---
+++ NLP/Lab9.py
@@ -0,0 +1,13 @@
+# Student Name:   Onur Onel
+# Student No:     0865803
+# Trent Email:    onuronel@trentu.ca
+# Date:           07.03.2025
+# Lab 9:          Natural Language Processing
+
+import nltk
+from nltk.corpus import gutenberg, stopwords
+
+
+macbeth_text = gutenberg.raw('shakespeare-macbeth.txt')
+
+print(macbeth_text)
